d28c67c409b2b8d1a26d0e658225d1bd396b2994
İsmail Özcan <50797929+Isso-Chan@users.noreply.github.com>
2020-05-13 16:36:42 +0200

    Update login.java

--- src/day_1/login.java
+++ src/day_1/login.java
@@ -5,5 +5,12 @@ public class login {
     public static void main(String[] args) {
 
         System.out.println();
+        
+        
+        
+        
+        
+        
+        //This part is added in Internet
     }
 }
